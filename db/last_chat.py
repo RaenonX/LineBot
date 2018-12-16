8bfc02a9cfd6f620f01648175d6f997794654259
RaenonX JELLYCAT <maplestoryxenon20130710@gmail.com>
2018-12-15 18:32:16 -0800

    debug

--- db/last_chat.py
+++ db/last_chat.py
@@ -46,7 +46,7 @@ class last_chat_recorder(db_base):
         s = ""
 
         if d is not None:
-            for uid, ts in d.items():
+            for uid, ts in d[last_chat_data.TIMESTAMP].items():
                 s += "{},{}".format(uid, ts.strftime('%Y/%m/%d %H:%M:%S'))
 
         return s
