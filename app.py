4b00a18a6c3c66eb57e1d8ceca366ef70aaa1bc3
RaenonX JELLYCAT <raenonx0710@gmail.com>
2019-04-12 20:35:53 -0700

    debug

--- app.py
+++ app.py
@@ -198,7 +198,7 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    print body
+    print body.encode('utf-8')
 
     # handle webhook body
     try:
